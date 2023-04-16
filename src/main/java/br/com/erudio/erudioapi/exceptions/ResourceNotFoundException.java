b57c263a0bf00cc898748ad5da538d570ad393d5
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2023-04-15 21:53:54 -0300

    fix: change UnsupportedMathExpression to ResourceNotFoundException

--- src/main/java/br/com/erudio/erudioapi/exceptions/ResourceNotFoundException.java
+++ src/main/java/br/com/erudio/erudioapi/exceptions/ResourceNotFoundException.java
@@ -3,9 +3,9 @@ package br.com.erudio.erudioapi.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UnsupportedMathOperationException extends RuntimeException {
-    public UnsupportedMathOperationException(String exception) {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String exception) {
         super(exception);
     }
 }
