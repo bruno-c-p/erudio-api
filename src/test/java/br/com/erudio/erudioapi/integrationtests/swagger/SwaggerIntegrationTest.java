93f475f9d12c5b7e1ce1ec8473619eb27410e645
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2023-04-17 23:28:41 -0300

    test: add swagger ui integration tests

--- src/test/java/br/com/erudio/erudioapi/integrationtests/swagger/SwaggerIntegrationTest.java
+++ src/test/java/br/com/erudio/erudioapi/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -1,13 +1,28 @@
 package br.com.erudio.erudioapi.integrationtests.swagger;
 
+import br.com.erudio.erudioapi.config.TestConfigs;
+import br.com.erudio.erudioapi.integrationtests.testcontainers.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class StartupTests {
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest{
     @Test
-    void contextLoads() {
+    public void shouldDisplaySwaggerUiPage() {
+        var content =
+                given()
+                        .basePath("/swagger-ui/index.html")
+                        .port(TestConfigs.SERVER_PORT)
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .asString();
+        assertTrue(content.contains("Swagger UI"));
     }
-
 }
--- src/test/java/br/com/erudio/erudioapi/integrationtests/testcontainers/AbstractIntegrationTest.java
+++ src/test/java/br/com/erudio/erudioapi/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -1,6 +1,5 @@
 package br.com.erudio.erudioapi.integrationtests.testcontainers;
 
-import br.com.erudio.erudioapi.config.TestConfigs;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -9,25 +8,18 @@ import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.lifecycle.Startables;
 
-import java.io.Serializable;
 import java.util.Map;
 import java.util.stream.Stream;
 
 @ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
 public class AbstractIntegrationTest {
-    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15");
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13.2");
 
         private static void startContainers() {
             Startables.deepStart(Stream.of(postgreSQLContainer)).join();
-        }
-
-        @Override
-        public void initialize(ConfigurableApplicationContext applicationContext) {
-            startContainers();
-            ConfigurableEnvironment environment = applicationContext.getEnvironment();
-            MapPropertySource testcontainers = new MapPropertySource("testcontainers", (Map) createConnectionConfiguration());
-            environment.getPropertySources().addFirst(testcontainers);
         }
 
         private static Map<String, String> createConnectionConfiguration() {
@@ -36,6 +28,17 @@ public class AbstractIntegrationTest {
                     "spring.datasource.username", postgreSQLContainer.getUsername(),
                     "spring.datasource.password", postgreSQLContainer.getPassword()
             );
+        }
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            startContainers();
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            MapPropertySource testcontainers = new MapPropertySource(
+                    "testcontainers",
+                    (Map) createConnectionConfiguration());
+            environment.getPropertySources().addFirst(testcontainers);
         }
     }
 }
